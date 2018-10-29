34dd40ffe5ba42921aa70a4ad464863745998db2
roy <roy@qq.com>
2018-10-29 16:33:29 +0800

    add package system

---
+++ Assets/PackageSystem/ItemBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace OneGame.Package
+{
+    public abstract class ItemBase
+    {
+        public abstract string Ids { get; set; }
+        public abstract string Name { get; set; }
+    }
+
+    public class Item : ItemBase
+    {
+        protected string mIds;
+
+        protected string mName;
+
+        public override string Ids
+        {
+            get{return mIds;}
+            set{mIds = value;}
+        }
+
+        public override string Name { get { return mName; } set { mName = value; } }
+
+        public Item(string ids)
+        {
+            mIds = ids;
+        }
+
+        public virtual bool Add2Package(Package package)
+        {
+            throw new NotImplementedException("Add2Package function has not been implemented, you should overide it in inherited class!");
+        }
+
+        public virtual bool RemoveFromPackage(Package package)
+        {
+            throw new NotImplementedException("Add2Package function has not been implemented, you should overide it in inherited class!");
+        }
+
+        public virtual bool ConsumeFromPackage(Package package)
+        {
+            throw new NotImplementedException("Add2Package function has not been implemented, you should overide it in inherited class!");
+        }
+    }
+}
+
---
+++ Assets/PackageSystem/Package.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace OneGame.Package
+{
+    [Serializable]
+    public class Package
+    {
+        private const int MaxCellCount = 99;
+        private const int MaxAbilityForEachCategory = 50;
+        List<ItemCell> AllItems = new List<ItemCell>();
+
+        public List<ItemCell> OneTimeUseItems { get; set; }
+        public List<ItemCell> Ingredients { get; set; }
+        public List<ItemCell> Skills { get; set; }
+
+        public void Add(Item itemToAdd,int count)
+        {
+            var result = AllItems.FindAll((i) => { return i.sItem.Ids == itemToAdd.Ids && i.sCount < 99; });
+            if (result != null && result.Count > 0)
+            {
+                if (result[0].sCount + count > MaxCellCount)
+                {
+                    result[0].sCount = MaxCellCount;
+                    AllItems.Add(new ItemCell() { sCount = result[0].sCount + count - MaxCellCount, sItem = itemToAdd, sType = result[0].sType });
+                }
+                else if (result[0].sCount + count == MaxCellCount)
+                {
+                    result[0].sCount = MaxCellCount;
+                }
+                else
+                {
+                    result[0].sCount += count;
+                }
+            }
+        }
+
+        public bool IfHas(string ids,int count)
+        {
+            
+            var results = AllItems.FindAll((i) => { return i.sItem.Ids == ids; });
+
+            int allCount = 0;
+
+            //add all item in all
+            foreach (var item in results)
+            {
+                allCount += item.sCount;
+            }
+
+            return allCount >= count ? false : true;
+
+        }
+    }
+
+    public class ItemCell
+    {
+        public Item sItem { get; set; }
+        public int sCount { get; set; }
+        public PackageCategory sType { get; set; }
+    }
+
+    public enum PackageCategory
+    {
+        None,
+        OneTimeUseItem,
+        Ingredients,
+        Skills,
+    }
+}
+
