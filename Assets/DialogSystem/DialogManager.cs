f1c4ea25343695c2239a396f9146d838478dbf7a
roy <roy@qq.com>
2018-11-06 17:03:27 +0800

    add dialog system

---
+++ Assets/DialogSystem/DialogManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public struct TextStruct
+{
+    public string Text;
+    public string FaceId;
+    public string SpeakerId;
+}
+
+public class DialogManager : MonoBehaviour
+{
+    public Transform DialogBox;
+    public Text DialogText;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public IEnumerator OpenDialog()
+    {
+
+        DialogBox.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.08f);
+    }
+
+    public IEnumerator CloseDialog()
+    {
+        DialogText.text = string.Empty;
+        DialogBox.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.08f);
+    }
+
+    public IEnumerator SetText(string text)
+    {
+        DialogText.text = string.Empty;
+        bool isRapidMode = false;
+        int count = 0;
+        while (count <= text.Length)
+        {
+            DialogText.text = text.Substring(0, count);
+            count++;
+            if (isRapidMode)
+            {
+                yield return new WaitForSeconds(0.01f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.02f);
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                isRapidMode = true;
+            }
+        }
+        while (true)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    public IEnumerator SetText(TextStruct data)
+    {
+        DialogText.text = string.Empty;
+        bool isRapidMode = false;
+        int count = 0;
+        while (count <= data.Text.Length)
+        {
+            DialogText.text = data.Text.Substring(0, count);
+            count++;
+            if (isRapidMode)
+            {
+                yield return new WaitForSeconds(0.01f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.02f);
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                isRapidMode = true;
+            }
+        }
+        while (true)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
--- Assets/InteractSystem/InteractResolver.cs
+++ Assets/InteractSystem/InteractResolver.cs
@@ -13,16 +13,25 @@ using System;
 public class InteractResolver : MonoBehaviour
 {
 
+
     InteractBase mInteractObj;
+
+    public DialogManager mDialogManager;
+
+    private bool isBusy = false;
 
     private IEnumerator Resolve()
     {
+        if (isBusy)
+            yield break;
+
         string source = mInteractObj.GetInteractText();
         if (string.IsNullOrEmpty(source))
             yield break;
 
-
-        StartCoroutine( Accept(source));
+        isBusy = true;
+        yield return StartCoroutine( Accept(source));
+        isBusy = false;
     }
 
     private IEnumerator Accept(string source)
@@ -38,23 +47,50 @@ public class InteractResolver : MonoBehaviour
             {
                 yield return StartCoroutine(CloseDialogBox());
             }
+            else if (tokens[i].StartsWith("Text"))
+            {
+                var values = tokens[i].Split('|');
+                var data = new TextStruct() { Text = values[1], SpeakerId = values[2], FaceId = values[3] };
+                yield return StartCoroutine(TextDisplay(data));
+            }
         }
     }
 
     private IEnumerator OpenDialogBox()
     {
-        throw new NotImplementedException();
+        yield return StartCoroutine(mDialogManager.OpenDialog());
     }
 
-    private IEnumerator TextDisplay(string text)
+    private IEnumerator TextDisplay(TextStruct data)
     {
-        throw new NotImplementedException();
+        yield return StartCoroutine(mDialogManager.SetText(data));
     }
+
 
     private IEnumerator CloseDialogBox()
     {
-        throw new NotImplementedException();
+        yield return StartCoroutine(mDialogManager.CloseDialog());
     }
 
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            var hits = Physics.RaycastAll(new Ray(transform.position, transform.forward), 10f, 1 << LayerMask.NameToLayer("Interaction"));
+            if (hits != null && hits.Length > 0)
+            {
+                mInteractObj = hits[0].collider.gameObject.GetComponent<InteractBase>();
+                if (mInteractObj!=null)
+                {
+                    StartCoroutine(Resolve());
+                }
+            }
+        }
+    }
 
 }
